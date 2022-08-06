edf71d1ff987929ff6a26439d6261d54669d63b5
Daniil <krtdaniil@gmail.com>
2022-08-06 20:13:24 +0300

    feat[task-5]: change csv import file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product:
     "https://rh9hxvvux3.execute-api.eu-west-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://4r2zmp72fk.execute-api.eu-west-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
